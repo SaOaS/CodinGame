19c188183d940b8a262fd40a21dc3171acaa7247
s-ahmedouali <souhail.market@gmail.com>
2015-03-18 12:00:54 +0100

    codingame

--- src/main/java/com/saoas/codingame/lecadeau/Solution.java
+++ src/main/java/com/saoas/codingame/lecadeau/Solution.java
@@ -6,13 +6,17 @@ import java.util.Scanner;
  * Created by s.ahmedouali on 18/03/2015.
  */
 class Solution {
+    //private static final Logger LOG = LogManager.getLogger(Solution.class);
 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
+        System.err.println("le nombre de Oods participant au cadeau : " + N);
         int C = in.nextInt();
+        System.err.println("le prix du cadeau : " + C);
         for (int i = 0; i < N; i++) {
             int B = in.nextInt();
+            System.err.println("Budget" + (i + 1) + " : " + B);
         }
 
         // Write an action using System.out.println()
