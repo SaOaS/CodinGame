780a73f09a48ec97d6a013159b1a144ffd39a9c3
s-ahmedouali <souhail.market@gmail.com>
2015-03-18 11:43:49 +0100

    init codingame

---
+++ src/main/java/com/saoas/codingame/lecadeau/Solution.java
@@ -0,0 +1,23 @@
+package com.saoas.codingame.lecadeau;
+
+import java.util.Scanner;
+
+/**
+ * Created by s.ahmedouali on 18/03/2015.
+ */
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int C = in.nextInt();
+        for (int i = 0; i < N; i++) {
+            int B = in.nextInt();
+        }
+
+        // Write an action using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println("IMPOSSIBLE");
+    }
+}
